c1cf3267f66db7276a5ee094e12e5b2b6194a5fb
dan.sve <daniel.svensson@utb.ecutbildning.se>
2022-06-21 12:50:51 +0200

    Listscreen array

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import ListpostsScreen from './screens/ListpostsScreen';
+import CreatepostScreen from './screens/CreatepostScreen';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={HomeScreen}/>
+        <Tabs.Screen name="List posts" component={ListpostsScreen}/>
+        <Tabs.Screen name="Create post" component={CreatepostScreen}/>
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
